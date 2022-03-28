84ac15b8cacf08866c89479ea8a4b2fe4c81cc4b
trigi1 <trigintillion1@gmail.com>
2022-03-28 16:01:57 -0400

    Add working text field + adds item on enter key up.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function createNewElement() {
   var newElement = document.createElement("li");
-  newElement.innerHTML = "thing";
+  newElement.innerHTML = document.getElementById("in").value;
+  document.getElementById("in").value = "";
   document.getElementById("list").appendChild(newElement);
+}
+
+function handler(event) {
+  if(event.keyCode == 13) {
+    event.preventDefault();
+    createNewElement();
+  }
 }
