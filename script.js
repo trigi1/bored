cb3604bb9c47bc6d52933292fd8d12d661934707
trigi1 <trigintillion1@gmail.com>
2022-03-28 19:27:07 -0400

    Add removeLastElement function

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
+let itemCount = 0;
+
 function createNewElement() {
+  itemCount++;
   var newElement = document.createElement("li");
   newElement.innerHTML = document.getElementById("in").value;
   newElement.className = "left";
+  newElement.id = "listItem" + itemCount;
   document.getElementById("in").value = "";
   document.getElementById("list").appendChild(newElement);
 }
 
+function removeLastElement() {
+  if(itemCount == 0) {
+    return;
+  }
+  var fullList = document.getElementById("list");
+  var removable = document.getElementById("listItem" + itemCount);
+  fullList.removeChild(removable);
+  var currentVal = document.getElementById("in").value;
+  document.getElementById("in").value = currentVal.substring(0, currentVal.length - 1);
+  itemCount--;
+}
+
 function handler(event) {
-  if(event.keyCode == 13) {
-    event.preventDefault();
-    createNewElement();
+  switch(event.key) {
+    case "Enter":
+      event.preventDefault();
+      createNewElement();
+      break;
+    case "\\":
+      event.preventDefault();
+      removeLastElement();
   }
 }
